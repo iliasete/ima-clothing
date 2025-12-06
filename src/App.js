86f1714838c715e2b80d3f5f7679c54fa51f9dc4
Ilias Moreno <iliasete@gmail.com>
2025-12-06 16:10:55 +0100

    adds routing and navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import Menu from './components/menu/menu.component.jsx';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title: 'hats',
-            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-        },
-        {
-            id: 2,
-            title: 'jackets',
-            imageUrl: 'https://i.ibb.co/px2tCc3/jakets.png',
-        },
-        {
-            id: 3,
-            title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-        },
-        {
-            id: 4,
-            title: 'womens',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-        },
-        {
-            id: 5,
-            title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-        },
-    ];
-
-    return <Menu categories={categories} />;
+    return (
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
