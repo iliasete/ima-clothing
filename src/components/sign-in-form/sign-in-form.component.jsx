eba936ecd7d3b4ce7ae1f4328072dba5409dc184
Ilias Moreno <iliasete@gmail.com>
2025-12-21 12:42:38 +0100

    finish authentication component

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInAuthUserWithEmailAndPassword,
+} from '../../utils/firebase/firebase.utils';
+
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import './sign-in-form.styles.scss';
+
+const defaultFormFields = {
+    email: '',
+    password: '',
+};
+
+const SignInForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { email, password } = formFields;
+
+    const signInWithGoogle = async () => {
+        const { user } = await signInWithGooglePopup();
+        await createUserDocumentFromAuth(user);
+    };
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormFields({ ...formFields, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await signInAuthUserWithEmailAndPassword(
+                email,
+                password
+            );
+            console.log(response);
+            resetFormFields();
+        } catch (error) {
+            if (error.code === 'auth/invalid-credential') {
+                alert('Incorrect email or password');
+            }
+        }
+    };
+
+    return (
+        <div className="sign-in-container">
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password</span>
+
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    label="Email"
+                    type="email"
+                    onChange={handleChange}
+                    name="email"
+                    value={email}
+                    required
+                />
+
+                <FormInput
+                    label="Password"
+                    type="password"
+                    onChange={handleChange}
+                    name="password"
+                    value={password}
+                    required
+                />
+                <div className="buttons-container">
+                    <Button type="submit">Sign In</Button>
+                    <Button
+                        type="button"
+                        buttonType="google"
+                        onClick={signInWithGoogle}>
+                        Sign in with google
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SignInForm;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -62,4 +63,11 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
     if (!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    console.log(`sign in with ${email} and ${password}`);
+    if (!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
 };
